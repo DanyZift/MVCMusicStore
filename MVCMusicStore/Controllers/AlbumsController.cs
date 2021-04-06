27c92708f629a29d629390f8014f2a8ea2fb7911
afitzgerald <afitzgerald@groupon.com>
2021-04-06 21:59:41 +0100

    chore: add in call to web api

---
+++ MVCMusicStore/Controllers/AlbumsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCMusicStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCMusicStore.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //Call our web api here
+            // URL: https://localhost:5001/api/album
+            IEnumerable<Album> albums = null;
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/api/");
+                var responseTask = client.GetAsync("album");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readJob = result.Content.ReadAsAsync<IList<Album>>();
+                    readJob.Wait();
+                    albums = readJob.Result;
+                }else
+                {
+                    //return error or empty value
+                    albums = Enumerable.Empty<Album>();
+                    ModelState.AddModelError(string.Empty, "No Albums were found!");
+                }
+
+            }
+
+            return View(albums);
+
+        }
+    }
+}
--- MVCMusicStore/wwwroot/js/site.js
+++ MVCMusicStore/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+(function () {
+
+
+
+    var url = 'https://localhost:5001/api/album';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data));
+
+
+
+
+
+}());
